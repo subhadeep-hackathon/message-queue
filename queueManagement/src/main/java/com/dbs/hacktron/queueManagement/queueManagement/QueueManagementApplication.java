9bc03797fbb334748a82861a2a1b8814f7412d3c
shikapoor <kapoorshivani856@gmail.com>
2019-08-10 11:57:57 +0530

    first commit

---
+++ queueManagement/src/main/java/com/dbs/hacktron/queueManagement/queueManagement/QueueManagementApplication.java
@@ -0,0 +1,13 @@
+package com.dbs.hacktron.queueManagement.queueManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueManagementApplication.class, args);
+	}
+
+}
